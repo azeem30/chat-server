2d710016806dea70ed1438267c169a15e0c46167
Azeem Pinjari <pinjariashpak3@gmail.com>
2025-09-06 17:23:24 +0530

    Remove unused imports

--- src/main/java/com/example/server/repository/MessageRepository.java
+++ src/main/java/com/example/server/repository/MessageRepository.java
@@ -5,13 +5,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.example.server.entity.Message;
-
-import jakarta.transaction.Transactional;
 
 public interface MessageRepository extends JpaRepository<Message, BigInteger> {
 
