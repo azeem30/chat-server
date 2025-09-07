511d38df8daac01d5258caa9b8404d653de4fae9
Azeem Pinjari <pinjariashpak3@gmail.com>
2025-09-07 17:56:59 +0530

    Fix errors

--- src/main/java/com/example/server/service/UserService.java
+++ src/main/java/com/example/server/service/UserService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.server.entity.User;
-import com.example.server.repository.MessageRepository;
 import com.example.server.repository.UserRepository;
 import com.example.server.utils.Cryptography;
 
@@ -17,6 +16,8 @@ import jakarta.transaction.Transactional;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private Cryptography cryptography;
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
@@ -24,7 +25,7 @@ public class UserService {
 
     public Boolean findUserByUsername(String username) {
         try {
-            username = Cryptography.compress(Cryptography.encrypt(username));
+            username = cryptography.compress(cryptography.encrypt(username));
             Optional<User> optionalUser = userRepository.findById(username);
             if (optionalUser.isEmpty()) {
                 return false;
@@ -38,8 +39,8 @@ public class UserService {
 
     public Boolean registerUser(User user) {
         try {
-            user.setUsername(Cryptography.compress(Cryptography.encrypt(user.getUsername())));
-            user.setPassword(Cryptography.compress(Cryptography.encrypt(user.getPassword())));
+            user.setUsername(cryptography.compress(cryptography.encrypt(user.getUsername())));
+            user.setPassword(cryptography.compress(cryptography.encrypt(user.getPassword())));
             User savedUser = userRepository.save(user);
             return savedUser != null;
         } catch (Exception e) {
@@ -50,13 +51,13 @@ public class UserService {
 
     public Boolean loginUser(User user) {
         try {
-            user.setUsername(Cryptography.compress(Cryptography.encrypt(user.getUsername())));
+            user.setUsername(cryptography.compress(cryptography.encrypt(user.getUsername())));
             Optional<User> optionalUser = userRepository.findById(user.getUsername());
             if (optionalUser.isEmpty()) {
                 return false;
             }
             User savedUser = optionalUser.get();
-            String decryptedPassword = Cryptography.decrypt(Cryptography.decompress(savedUser.getPassword()));
+            String decryptedPassword = cryptography.decrypt(cryptography.decompress(savedUser.getPassword()));
             return user.getPassword().equals(decryptedPassword) && user.getIp().equals(savedUser.getIp());
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -67,8 +68,8 @@ public class UserService {
     @Transactional
     public Boolean updateUsername(String oldUsername, String newUsername) {
         try {
-            String encryptedOldUsername = Cryptography.compress(Cryptography.encrypt(oldUsername));
-            String encryptedNewUsername = Cryptography.compress(Cryptography.encrypt(newUsername));
+            String encryptedOldUsername = cryptography.compress(cryptography.encrypt(oldUsername));
+            String encryptedNewUsername = cryptography.compress(cryptography.encrypt(newUsername));
             userRepository.updateUsername(encryptedOldUsername, encryptedNewUsername);
             return true;
         } catch (Exception e) {
@@ -79,9 +80,9 @@ public class UserService {
 
     public Boolean updatePassword(String username, String oldPassword, String newPassword) {
         try {
-            String encryptedUsername = Cryptography.compress(Cryptography.encrypt(username));
-            String encryptedOldPassword = Cryptography.compress(Cryptography.encrypt(oldPassword));
-            String encryptedNewPassword = Cryptography.compress(Cryptography.encrypt(newPassword));
+            String encryptedUsername = cryptography.compress(cryptography.encrypt(username));
+            String encryptedOldPassword = cryptography.compress(cryptography.encrypt(oldPassword));
+            String encryptedNewPassword = cryptography.compress(cryptography.encrypt(newPassword));
             userRepository.updatePassword(encryptedUsername, encryptedOldPassword, encryptedNewPassword);
             return true;
         } catch (Exception e) {
