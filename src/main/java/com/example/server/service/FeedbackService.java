6ba3c17e1a7e5b9523b5df87be08a1e07b97f4f4
Azeem Pinjari <pinjariashpak3@gmail.com>
2025-09-08 12:40:55 +0530

    Fix errors

--- src/main/java/com/example/server/service/FeedbackService.java
+++ src/main/java/com/example/server/service/FeedbackService.java
@@ -3,11 +3,13 @@ package com.example.server.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.server.entity.Feedback;
 import com.example.server.repository.FeedbackRepository;
 import com.example.server.utils.Cryptography;
 
+@Service
 public class FeedbackService {
     private static final Logger logger = LoggerFactory.getLogger(FeedbackService.class);
 
