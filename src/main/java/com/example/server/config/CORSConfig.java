83b6062db5cc587f6ab0310a3513b7bfdb3ddab9
Azeem Pinjari <113772951+azeem30@users.noreply.github.com>
2025-09-08 20:37:25 +0530

    Update CORS Configuration

--- src/main/java/com/example/server/config/CORSConfig.java
+++ src/main/java/com/example/server/config/CORSConfig.java
@@ -9,7 +9,7 @@ public class CORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("")
+                .allowedOrigins("https://feedback-page-lilac.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
